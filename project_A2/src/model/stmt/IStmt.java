99a3f29fe8f11eb5c18258284c200cbc8241c6e7
Carp Nik <115424518+NikAlien@users.noreply.github.com>
2024-03-25 23:32:15 +0200

    Delete project_A2 directory

--- project_A2/src/model/stmt/IStmt.java
+++
@@ -1,7 +0,0 @@
-package model.stmt;
-import model.PrgState;
-import controller.MyException; // We'll see with exceptions ??/
-
-public interface IStmt {
-    PrgState execute(PrgState state) throws MyException;
-}
--- project_A2/src/repository/IRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-import model.PrgState;
-public interface IRepository {
-    PrgState getCrtPrg();
-
-    void add(PrgState program);
-}
